bf012e6feb14b5c4997be546236afa3e318874e3
Aaron Zheng <153031902+aaronxyzheng@users.noreply.github.com>
2026-02-22 08:56:48 -0500

    feat: create basic page layout

---
+++ src/components/Resume.jsx
@@ -0,0 +1,19 @@
+import "./Resume.css"
+
+function Resume() {
+    return (
+        <div id="resume">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna tellus, rhoncus sed nisl ut, iaculis maximus est. Phasellus ac posuere tortor, sit amet hendrerit dui. Maecenas quis commodo lectus, et sollicitudin tortor. Sed sit amet lacinia lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed at ex iaculis, maximus quam a, lacinia eros. Duis faucibus ullamcorper tortor, non finibus elit accumsan quis. Donec et orci eu enim aliquet dictum in ac mauris.
+
+Maecenas eget vehicula felis, at porttitor libero. Nulla sit amet porttitor nibh, id pharetra dolor. Vestibulum sagittis nulla nisi, id auctor dolor maximus tempor. Cras sit amet arcu ullamcorper, rhoncus dolor eleifend, vestibulum eros. Proin vel placerat metus. Curabitur tristique, velit at imperdiet tristique, nulla metus venenatis eros, at tristique risus nibh non justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus porta nulla a blandit elementum. Proin a est in nibh molestie tempor. Nulla sodales, lorem quis pretium vulputate, lorem nulla convallis enim, id dignissim sapien ligula eu sapien. Aliquam erat volutpat.
+
+Proin imperdiet mollis nibh in euismod. Phasellus quis egestas velit. Proin semper felis sit amet urna tristique accumsan. Praesent tortor enim, aliquet at mollis eget, sodales sed massa. Integer vitae turpis a lorem vestibulum fringilla nec egestas ligula. Aliquam sollicitudin ac augue pulvinar mollis. Nunc vel neque eget orci malesuada blandit. Mauris hendrerit quis enim vitae malesuada. Maecenas varius auctor lorem sit amet fermentum.
+
+Sed tincidunt vulputate mollis. Vivamus at quam augue. Fusce viverra feugiat lorem et accumsan. Suspendisse potenti. Aliquam mollis, erat eu malesuada dapibus, ipsum eros imperdiet ligula, eu lacinia est tellus in nunc. Donec aliquam efficitur massa, sit amet congue neque maximus ac. Phasellus varius interdum porta. Praesent viverra quam arcu, ut pulvinar nisl mattis et. Ut ultricies ornare fringilla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+Suspendisse vitae ante varius velit aliquam mollis. Integer rhoncus elit vitae urna lobortis, eget iaculis turpis iaculis. Vivamus eu metus posuere, sodales neque id, luctus massa. Cras facilisis mauris consectetur lectus ornare, at posuere justo hendrerit. Ut magna ipsum, rutrum eu sodales cursus, accumsan id mauris. Vivamus lorem urna, porttitor ac justo vitae, accumsan placerat diam. In nec nisl a tellus viverra mollis. Cras congue massa ut ex egestas, vel consequat elit convallis. Donec non auctor orci. Donec tristique mauris mi, non auctor neque ultrices ut.
+        </div>
+    )
+}
+
+export default Resume;
---
+++ src/components/ResumeBuilder.jsx
@@ -0,0 +1,11 @@
+import "./ResumeBuilder.css"
+
+function ResumeBuilder() {
+    return (
+        <div id="resume-builder">
+            Resume Builder
+        </div>
+    )
+}
+
+export default ResumeBuilder;
