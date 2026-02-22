1ab38ad1a0c5baf0f360d9cab896bdfb99d4bf2f
Aaron Zheng <153031902+aaronxyzheng@users.noreply.github.com>
2026-02-22 16:27:59 -0500

    feat: working personal information section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import Resume from './components/Resume'
-import ResumeBuilder from './components/ResumeBuilder'
+import Resume from "./components/Resume";
+import ResumeBuilder from "./components/ResumeBuilder";
+import { useState } from "react";
 
 function App() {
+    const [info, setInfo] = useState({
+        personalInfo: {
+            name: "",
+            email: "",
+            phoneNumber: "",
+            website: "",
+            location: "",
+        },
+    });
 
-  return (
-    <>
-      <Resume/>
-      <ResumeBuilder/>
-    </>
-  )
+    const example = {
+        personalInfo: {
+            name: "Aaron Zheng",
+            email: "aaronxyzheng@gmail.com",
+            phoneNumber: "+1 914-610-6511",
+            website: "aaronxyzheng.com",
+            location: "Briarcliff, New York",
+        },
+    };
+
+    function updatePersonalInfo(field, value) {
+        setInfo((prev) => ({ ...prev, personalInfo: { ...prev.personalInfo, [field]: value } }));
+    }
+
+    return (
+        <>
+            <Resume info={info} />
+            <ResumeBuilder updatePersonalInfo={updatePersonalInfo} />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/ResumeBuilder.jsx
+++ src/components/ResumeBuilder.jsx
@@ -1,11 +1,49 @@
-import "./ResumeBuilder.css"
+import "./ResumeBuilder.css";
 
-function ResumeBuilder() {
+function ResumeBuilder({ updatePersonalInfo }) {
     return (
         <div id="resume-builder">
-            Resume Builder
+            <div id="button-div">
+                <button>Clear</button>
+                <button>Load Example</button>
+            </div>
+            <div id="personal-information">
+                <h1>Personal Information</h1>
+                <div>
+                    <label htmlFor="name-input">Full Name</label>
+                    <input id="name-input" type="text" onChange={(e) => updatePersonalInfo("name", e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="email-input">Email</label>
+                    <input id="email-input" type="text" onChange={(e) => updatePersonalInfo("email", e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="phone-number-input">Phone Number</label>
+                    <input
+                        id="phone-number-input"
+                        type="text"
+                        onChange={(e) => updatePersonalInfo("phoneNumber", e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="website-input">Website</label>
+                    <input
+                        id="website-input"
+                        type="text"
+                        onChange={(e) => updatePersonalInfo("website", e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="location-input">Location</label>
+                    <input
+                        id="location-input"
+                        type="text"
+                        onChange={(e) => updatePersonalInfo("location", e.target.value)}
+                    />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ResumeBuilder;
